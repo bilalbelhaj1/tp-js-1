62fe13bee99ac2c4e246ed03e179b188b0bf9c03
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2025-04-19 14:01:41 +0100

    finale commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// TODO : Ajouter un écouteur d'événement sur le formulaire 
+document.getElementById('form-examen').addEventListener('submit', function(e) 
+{ 
+    e.preventDefault(); 
+
+    // TODO : Récupérer les valeurs du formulaire 
+    const examen = {
+        id: document.getElementById('cni').value,
+        nom: document.getElementById('nom').value, 
+        duree: parseInt(document.getElementById('duree').value), 
+        description: document.getElementById('description').value, 
+        proprietaire: document.getElementById('proprietaire').value,
+        questions: [] 
+    }; 
+    console.log(examen);
+
+// TODO : Sauvegarder l'examen dans le localStorage sous une clé basée sur  le nom du propriétaire 
+const examsKey = 'examens_' + examen.proprietaire; 
+const exams = JSON.parse(localStorage.getItem(examsKey)) || []; 
+exams.push(examen); 
+localStorage.setItem(examsKey, JSON.stringify(exams)); 
+
+alert('Examen ajouté avec succès !'); 
+this.reset(); 
+}); 
